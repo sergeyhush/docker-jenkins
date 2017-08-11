d6a0f5ba8884c793dd46af454fd74513e87d56b5
Sergey Sudakovich <sergey@cumulusnetworks.com>
2017-08-11 11:38:33 -0700

    Add timeout

--- master/test_master.py
+++ master/test_master.py
@@ -4,12 +4,8 @@ import requests
 
 @pytest.fixture()
 def jenkins_url():
-    return "http://127.0.0.1:80"
-
-
-def is_url_up(u):
-    return requests.get(u).status_code == requests.codes.ok
+    return "http://127.0.0.1/api/json"
 
 
 def test_is_online(jenkins_url):
-    assert is_url_up(jenkins_url)
+    requests.get(jenkins_url, timeout=5).raise_for_status()
